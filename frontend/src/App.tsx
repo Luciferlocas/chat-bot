ab20a0f6db9cfe4559fb80e72fe86aacab7f2731
Luciferlocas <rohitsomvanshi2003@gmail.com>
2025-12-27 16:50:26 +0530

    feat: Add instructions to access the assistant

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,14 @@ import Widget from "./components/chat/widget";
 
 export default function App() {
   return (
-    <div className="min-h-dvh relative overflow-hidden">
+    <div className="min-h-dvh flex items-center justify-center relative overflow-hidden">
+      <div className="flex flex-col items-center text-center font-mono p-4 gap-2">
+        <h1 className="text-2xl md:text-5xl font-black">TechNova</h1>
+        <p className="text-sm md:text-lg max-w-xl">
+          If you have any questions, please feel free to ask the assistant at
+          the bottom right of the page.
+        </p>
+      </div>
       <Widget />
     </div>
   );
